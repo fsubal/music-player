f9f69dd882f976b162d0b6d4f766dd0d8fab5c2b
fsubal <fsubal1102@gmail.com>
2020-03-01 23:33:40 +0900

    use custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="https://unpkg.com/marx-css/css/marx.min.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,13 @@ interface Props {
   userAgent: string
 }
 
-const RootIndex: NextPage<Props> = ({ userAgent }) => <h1>Hello world! - user agent: {userAgent}</h1>
+const RootIndex: NextPage<Props> = ({ userAgent }) => (
+  <main>
+    <h1>Hello world!</h1>
+    <p>user agent: {userAgent}</p>
+    <footer>&copy; nothing</footer>
+  </main>
+)
 
 RootIndex.getInitialProps = async ({ req }): Promise<Props> => {
   const userAgent = req ? req.headers['user-agent'] ?? '' : navigator.userAgent

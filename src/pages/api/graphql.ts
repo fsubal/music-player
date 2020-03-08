fea80dfdef5bc497da197e00c063764e7a4ba967
fsubal <fsubal1102@gmail.com>
2020-03-08 21:15:44 +0900

    connect to graphql

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,8 +1,45 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
+import airtable, { toEntity } from '../../lib/airtable'
 
 const typeDefs = gql`
+  type Thumbnail {
+    url: String!
+    width: Int!
+    height: Int!
+  }
+
+  type Thumbnails {
+    small: Thumbnail
+    large: Thumbnail
+    full: Thumbnail
+  }
+
+  type Attachment {
+    id: String!
+    url: String!
+    filename: String!
+    size: Int!
+    type: String!
+    thumbnails: Thumbnails
+  }
+
+  type Track {
+    name: String!
+    albums: [ID]!
+    track: String!
+    side: String!
+    mustHearTracks: Boolean
+    specificInstrumentalCredit: String
+  }
+
+  type Album {
+    albumTitle: String
+    albumCovers: [Attachment]!
+    tracks: [ID]!
+  }
+
   type Query {
-    sayHello: String
+    albums: [Album]!
   }
 `
 
@@ -10,8 +47,22 @@ const apolloServer = new ApolloServer({
   typeDefs,
   resolvers: {
     Query: {
-      sayHello() {
-        return 'Hello World!'
+      async albums() {
+        return airtable('Albums')
+          .select()
+          .all()
+          .then(records => records.map(toEntity))
+      },
+    },
+    Album: {
+      albumTitle(album: any) {
+        return album['Album Title']
+      },
+      albumCovers(album: any) {
+        return album['Album Cover(s)']
+      },
+      tracks(album: any) {
+        return album['Tracks']
       },
     },
   },
--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -4,9 +4,9 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
     const rows = (
-      await airtable('David Bowie 1969-1983: The Golden Years')
+      await airtable('Albums')
         .select()
-        .firstPage()
+        .all()
     ).map(toEntity)
 
     res.status(200).json(rows)

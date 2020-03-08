8a3621c7c7d764e15255ab31277fdba32a8c44fc
fsubal <fsubal1102@gmail.com>
2020-03-09 01:31:17 +0900

    dont use absolute url

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -4,6 +4,6 @@ import ApolloClient from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 export default new ApolloClient({
-  link: createHttpLink({ uri: 'http://localhost:3000/api/graphql' }),
+  link: createHttpLink({ uri: 'api/graphql' }),
   cache: new InMemoryCache(),
 })

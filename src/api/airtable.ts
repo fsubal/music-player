9493264802fc4fc119ca62c32d3037355619d337
fsubal <fsubal1102@gmail.com>
2020-03-08 19:00:58 +0900

    fetch first pages

--- src/api/airtable.ts
+++ src/api/airtable.ts
@@ -5,4 +5,10 @@ const airtable = new Airtable({
   apiKey: process.env.AIRTABLE_API_KEY!,
 })
 
+/**
+ * DON'T import this in client side ( it would fail ).
+ *
+ * - don't import this from /components
+ * - don't import this from /pages, except for /pages/api
+ */
 export default airtable.base(process.env.AIRTABLE_WORKSPACE_ID!)
--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,7 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const rows = await airtable('Video tracker').find('1')
+    const rows = await airtable('Video tracker')
+      .select()
+      .firstPage()
     res.status(200).json(rows)
   } catch (e) {
     res.status(e.statusCode).json({})

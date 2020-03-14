1a8c36db652c9ab746ec168c0114d0d9b08240e1
fsubal <fsubal1102@gmail.com>
2020-03-14 13:50:05 +0900

    fix deprecation error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import { ApolloProvider } from '@apollo/react-hooks'
 import apolloClient from '../lib/apollo-client'
 
@@ -8,11 +8,9 @@ export default class MyDocument extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <Container>
-        <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
-        </ApolloProvider>
-      </Container>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     )
   }
 }
